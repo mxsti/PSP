ddb380a6e8430f775bf6d6aadb23d61887834dd3
lukas <lukas@thinkpad>
2018-05-03 10:56:49 +0200

    test

--- Aufgabe 4/meth+func/src/main/scala/MethFuncService.scala
+++ Aufgabe 4/meth+func/src/main/scala/MethFuncService.scala
@@ -3,10 +3,13 @@ class MethFuncService {
   /** Liefert die p-te Potenz von b.
     *
     * @throws IllegalArgumentException b**p not representable in type Int */
-  def squareMultiplyMethod(b: Int, p: Int): Int =
+  def squareMultiplyMethod(b: Int, p: Int): Int = {
+    require(b >= 0, "b muss größer gleich 0 sein")
+    require(p >= 0, "p muss größer gleich 0 sein")
     if (p == 1) b
-    else if (p%2 != 0) b * squareMultiplyMethod(b,(p - 1))
-    else squareMultiplyMethod(b, p / 2) * squareMultiplyMethod(b, p / 2)
+    else if (p % 2 != 0) b * squareMultiplyMethod(b, (p - 1))
+    else (squareMultiplyMethod(b, p / 2) * squareMultiplyMethod(b, p / 2))
+  }
 
   /**Liefert die p-te Potenz von b.*/
   val squareMultiplyFunction: (Int,Int)=>Int = {
@@ -15,9 +18,15 @@ class MethFuncService {
 
   /** Computes the area between the X-axis and the graph of the function f from 0 to endX
     * by summing up the rectangles beneath the graph with a width of incrementX. */
-  def integrate(f: Double=>Double, endX: Double, incrementX: Double): Double = ???
-
-
+  def integrate(f: Double=>Double, endX: Double, incrementX: Double): Double = {
+    var x = incrementX
+    var result = 0.0
+    while (x <= endX){
+      result = result + (f(x) * incrementX)
+      x = x + incrementX
+    }
+    return result
+  }
 
 
 }
--- Aufgabe 4/meth+func/src/main/scala/TestMethFunc.scala
+++ Aufgabe 4/meth+func/src/main/scala/TestMethFunc.scala
@@ -3,9 +3,9 @@ object TestMethFunc {
     def main(args: Array[String]) {
       //Methode
       val testObjekt = new MethFuncService
-      println(testObjekt.squareMultiplyMethod(3,3))
+      println(testObjekt.squareMultiplyMethod(-2,30))
       //Funktion
       val f = testObjekt.squareMultiplyFunction
-      println(f(2,16))
+      println(f(2,5))
     }
 }
