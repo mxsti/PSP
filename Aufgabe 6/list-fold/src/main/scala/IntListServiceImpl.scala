f89666ad52e35f2ac676750c0b1ed88c410ee58e
lukas <lukas@thinkpad>
2018-05-24 11:09:37 +0200

    Korrektur

--- Aufgabe 6/list-fold/src/main/scala/IntListServiceImpl.scala
+++ Aufgabe 6/list-fold/src/main/scala/IntListServiceImpl.scala
@@ -2,26 +2,21 @@ class IntListServiceImpl extends IntListService {
 
   def containedIn(value: Int, list: List[Int]): Boolean = {
     list.foldLeft(false) { (notContaining,i) =>
-      if (i == value){
-        return true
-      }
-    notContaining
+      if (i == value) return true
+      notContaining
     }
   }
 
   def removeFrom(value: Int, list: List[Int]): List[Int] = {
-    list.foldLeft(List[Int]()) { (resultList,i) =>
-      if (i != value){
-        resultList :+ i
-      }else {
-        resultList
-      }
+    list.foldRight(List[Int]()) { (i, resultList) =>
+      if(i != value) i :: resultList
+      else resultList
     }
   }
 
   def concatenate(head: List[Int], tail: List[Int]): List[Int] = {
-    tail.foldLeft(head) { (resultList,i) =>
-      resultList :+ i
+    head.foldRight(tail) { (i, resultList) =>
+      i :: resultList
     }
   }
 
