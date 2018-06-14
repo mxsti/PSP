3345c6e2ee9ee9a5d850283c966157bc9cfb1c7e
lukas <lukas@thinkpad>
2018-06-14 11:11:14 +0200

    fertig

--- Aufgabe 9/actor-ping-pong/src/main/scala/Forwarder.scala
+++ Aufgabe 9/actor-ping-pong/src/main/scala/Forwarder.scala
@@ -3,24 +3,24 @@ import akka.actor.{Actor, ActorLogging, Props, ActorRef}
 object Forwarder {
 
     /**Returns a Props object for creation of a Reflector actor.*/
-    def props: Props = Props(new Forwarder)
+    def props(reflector: ActorRef): Props = Props(new Forwarder(reflector))
 
     /**The message types an actor can understand are usually defined in his companion object as case classes.*/
     case class Ping(id: Int)
 
 }
 
-class Forwarder extends Actor with ActorLogging {
+class Forwarder(reflector: ActorRef) extends Actor with ActorLogging {
 
   import Forwarder._
   private var count = 0
-  val receiver: ActorRef = context.actorOf(Props[Reflector])
 
   def receive: Receive = {
     case p: Ping =>
       count += 1
-      log.debug(s"Received $p as #$count.")
-      receiver ! Reflector.Pang(p.id)
+      println(s"Received $p as #$count.")
+      reflector ! Reflector.Pang(p.id)
+      reflector ! Reflector.Ref(sender())
   }
 
 }
