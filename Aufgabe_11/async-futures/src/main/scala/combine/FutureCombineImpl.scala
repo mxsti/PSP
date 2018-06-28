e7b618a3d8650886be224a9dcc8035d1301fa2dc
lukas <lukas@thinkpad>
2018-06-28 11:25:53 +0200

    :(

--- Aufgabe_11/async-futures/src/main/scala/combine/FutureCombineImpl.scala
+++ Aufgabe_11/async-futures/src/main/scala/combine/FutureCombineImpl.scala
@@ -1,24 +1,29 @@
 package combine
 
 import scala.concurrent.{Future, Promise}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class FutureCombineImpl extends FutureCombine {
   /** Liefert eine Future mit dem Ergebnis bzw. Versagen der als erste terminierenden Future aus {futures}.
     * Die Ergebnisse aller anderen Futures aus {futures} werden ignoriert. */
   override def firstOf[T](futures: Seq[Future[T]]): Future[T] = {
-    val prom: Promise[T] = Promise()
+    val promise: Promise[T] = Promise()
     futures.foreach(_.onComplete {
-      //Ergebnis des Future?
-     _.get
+      promise.tryComplete
     })
-    ???
+    promise.future
   }
 
   /** Liefert eine Future mit einer Sequenz der erfolgreichen Ergebnisse aller Futures aus {futures}.
     * Wenn eine Future aus {futures} versagt oder nicht innerhalb des Timeout-Zeitraums terminiert,
     * versagt auch die gelieferte Future mit einer Exception. */
   override def allOf[T](futures: Seq[Future[T]]): Future[Seq[T]] = {
-    val prom = Promise[T] = Promise()
-    ???
+    val promise: Promise[T] = Promise()
+    val emptySeq = Seq[T]()
+    //val test: Future[Seq[T]] = Future(emptySeq)
+    futures.foldLeft(emptySeq) { (seq: Seq[T], currentFuture: Future[T]) =>
+      ???
+    }
   }
+
 }
